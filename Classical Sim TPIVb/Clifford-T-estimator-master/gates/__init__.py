851defb8676225ec53608af3911adf7f7e4362a2
Dheasra <rezad@hotmail.ch>
2022-05-30 09:44:22 +0200

    added report, removed some unrelevant files

--- Classical Sim TPIVb/Clifford-T-estimator-master/gates/__init__.py
+++
@@ -1,2 +0,0 @@
-from .base import Gate, TGate, CompositeGate
-from .cliffords import SGate, CXGate, CZGate, HGate, CompositeCliffordGate, SwapGate, PauliZProjector, XGate
--- Classical Sim TPIVb/Clifford-T-estimator-master/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup, Extension
-import numpy
-
-setup(name = 'clifford_t_estim', version = '1.0',  \
-   ext_modules = [Extension('clifford_t_estim', ['cmodule.c', 'bitarray.c','CH.c','AG.c', "QCircuit.c"], extra_compile_args=["-O3"],include_dirs=[numpy.get_include()], libraries=["gsl", "gslcblas"])])
